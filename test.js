19ed4d79ce72647a709168d627810fa0e3dab612
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2023-10-04 21:04:54 +0100

    bugsFixed

--- test.js
+++ test.js
@@ -1,17 +1,20 @@
 // imort the pg package
-import { Pool } from "pg";
+import pg from "pg";
 
 // connect to the database
+//get the connection string
+const connectionString = process.env.DB_CONNECTION_STRING;
 
 
 //Create a new pool instance 
-const pool = new Pool ({
-    // Remember to pass in your connection details
+const pool = new pg.Pool ({connectionString
+    // Remember to pass in your connection details connectionString
 })
 
 //send a querry - SELECT * FROM BOOKS;
-const result = await pool.query("SELECT * FROM books;");
-console.log(result);
+const data = await pool.query("SELECT * FROM books;");
+const rows = data.rows;
+console.log(data);
 
 // close the connection
 await pool.end();
