daabc6d597df4d90e0c42af15548deafbe12fc01
Kveta Mooney <132582410+selfcoder-gazell@users.noreply.github.com>
2023-10-05 09:41:38 +0100

    fixedBugs

--- test.js
+++ test.js
@@ -9,12 +9,12 @@ const connectionString = process.env.DB_CONNECTION_STRING;
 //Create a new pool instance 
 const pool = new pg.Pool ({connectionString
     // Remember to pass in your connection details connectionString
-})
+});
 
 //send a querry - SELECT * FROM BOOKS;
-const data = await pool.query("SELECT * FROM books;");
+const data = await pool.query("SELECT * FROM authors;");
 const rows = data.rows;
-console.log(data);
+console.log(rows);
 
 // close the connection
 await pool.end();
